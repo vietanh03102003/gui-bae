93bccb2c7ccc8ba0d9090f574f560ddaaf565f54
vietanh03102003 <85342856+vietanh03102003@users.noreply.github.com>
2021-11-05 10:24:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,1 @@
-const CONFIG = {
-    introTitle: 'Hằng à!',
-    introDesc: `Biết lỗi của bản thân rồi 
-    Sẽ cố dành nhiều thời gian cho bé hơn
-    Không dừng lại nha `,
-    btnIntro: 'Nói được làm được ',
-    title: 'lần này biết lỗi thật rồi tha thứ nha ',
-    desc: 'Bé Yêu  ',
-    btnYes: 'Tha thứ cho lần này ',
-    btnNo: 'Không :( ( buồn lắm á )',
-    question:'sẽ dành thời gian cho bé nhiều hơn ',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Mong sẽ được bé tha thứ ',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 11h30 call nha bé .',
-    btnAccept: 'Okiiiii lun <3',
-    messLink:  ' https://www.facebook.com/vietanh0310 '
-}
+
